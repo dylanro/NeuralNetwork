587f27099b117208c27c266ed4b864fbb757c1df
Dylan Roberts <31771652+dylanro@users.noreply.github.com>
2018-05-15 20:53:54 -0500

    Delete data.js

--- NeuralNetwork/data.js
+++
@@ -1,8 +0,0 @@
-//Stores all the information for a single data sample
-class Data {
-  constructor() {
-    this.inputs = [];//stores every pixel, size = width * height
-    this.outputs = [];//size = 10 (0-9)
-    this.label;//what the Data sample actually is
-  }
-}
