d688f1dd55450a3309b439c302df5a00317d3426
Dylan Roberts <31771652+dylanro@users.noreply.github.com>
2018-05-16 23:05:44 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 let network;
+let trainTable;
 let trainingSet = [];//80%
+let testTable;
 let testingSet = [];//20%
+
+function preload() {
+    trainTable = loadTable('data/train.csv', 'csv', "header");
+    testTable = loadTable('data/test.csv', 'csv', "header");
+}
 
 function setup() {
     network = new NeuralNet(196, 49, 10);
+    print(table.getRowCount() + ' total rows in table');
+    print(table.getColumnCount() + ' total columns in table');
+    var tableArray = table.getArray();
+    print(tableArray);
     //load the data into trainingSet and testingSet
 }
 
